c1d6a3d87763ba402ee35368bf7aef1872dfd900
Art2233 <abiluba9@gmail.com>
2023-02-22 16:46:22 +0200

    3 Изменение

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const btn = document.querySelector('.btn');
+const p1 = document.querySelector('.p1');
 
 function clickBtn(event){
     if(event.target){
         console.log('ok');
+        p1.innerHTML = 'Кнопка была нажата!';
     }
 }
 
